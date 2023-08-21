830c5b949c7dc11c742f3181ff0d591b03808e1c
ESCADER, XYP COLLEN <116700665+Askelad777@users.noreply.github.com>
2023-08-21 18:28:10 +0800

    commit

--- functions-01-starting-code/app.js
+++ functions-01-starting-code/app.js
@@ -1,1 +1,73 @@
 const startGameBtn = document.getElementById('start-game-btn');
+
+const ROCK  = 'ROCK';
+const PAPER = 'PAPER';
+const SCISSOR = 'SCISSOR';
+const Default_player_choice = ROCK;
+let gameIsRunning = false;
+const RESULT_DRAW = 'DRAW';
+const RESULT_PLAYER_WINS = 'PLAYER WIN';
+const RESULT_COMPUTER_WINS = 'COMPUTER WIN';
+const RESULT_LOSE = 'LOSE';
+
+
+
+const GetPlayerChoice = function(){
+  const selection = prompt(`Choose ${ROCK}, ${PAPER} and, ${SCISSOR} to play.`).toUpperCase;
+  if(
+    selection !== ROCK ||
+    selection !== PAPER ||
+    selection !== SCISSOR 
+    ) {
+      alert(`Invalid choice, Player's choice back to default '${Default_player_choice}'`);
+      return Default_player_choice;
+  }{
+    return selection;
+  }
+}
+
+const getComputerChoice = function(){
+  const computerChoice = Math.random();
+  if(computerChoice < .34){
+    return ROCK;
+  }else if(computerChoice < .64){
+    return PAPER;
+  }else{
+    return SCISSOR;
+  }
+}
+
+
+const getWinner = function(cChoice, pChoice){
+  if(cChoice === pChoice){
+    return RESULT_DRAW;
+  }else if(
+      cChoice === ROCK && pChoice === PAPER ||
+      cChoice === PAPER&& pChoice === SCISSOR ||
+      cChoice === SCISSOR&& pChoice === ROCK
+
+  ){
+    return RESULT_PLAYER_WINS;
+  }else{
+    return RESULT_COMPUTER_WINS;
+  }
+
+}
+
+
+
+
+
+//startGame(); // work directly.
+
+startGameBtn.addEventListener('click', function(){
+    if(gameIsRunning){
+      return;
+    } 
+      gameIsRunning = true;
+      console.log('The game has start.....');
+      const playerChoice = GetPlayerChoice();
+      const computerChoice = getComputerChoice();
+      const winner = getWinner(computerChoice, playerChoice);
+      console.log(winner);
+});
